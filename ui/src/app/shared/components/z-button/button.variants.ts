87b7b4f5c11b548f3d33552423113dad009348d7
phun <phun5701@gmail.com>
2025-10-18 01:41:45 -0500

    Fixed borders and shadow

--- ui/src/app/shared/components/z-button/button.variants.ts
+++ ui/src/app/shared/components/z-button/button.variants.ts
@@ -5,13 +5,13 @@ export const buttonVariants = cva(
   {
     variants: {
       zType: {
-        default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
+        default: 'bg-primary text-primary-foreground shadow-xs/10 hover:bg-primary/90',
         destructive:
-          'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+          'bg-destructive text-white shadow-xs/10 hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
-          'border bg-secondary shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:hover:bg-input/50',
-        secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:bg-accent/50 dark:hover:bg-accent/50',
+          'border border-t-highlight bg-secondary shadow-xs/10 hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:hover:bg-input/50',
+        secondary: 'bg-secondary text-secondary-foreground shadow-xs/10 hover:bg-secondary/80',
+        ghost: 'hover:bg-background-mid hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       zSize: {
