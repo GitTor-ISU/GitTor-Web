d8c56ff71c412c0d47638aabdef7a58b355391b6
phun <125079268+phu-n@users.noreply.github.com>
2025-10-23 20:48:18 -0500

    Added zard layout

--- ui/src/app/layouts/main-layout/main-layout.ts
+++ ui/src/app/layouts/main-layout/main-layout.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '@features/navbar/navbar';
+import { LayoutModule } from '@shared/components/z-layout/layout.module';
 
 /**
  * Main layout component.
  */
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, LayoutModule],
   templateUrl: './main-layout.html',
 })
 export class MainLayout {}
