6ab3ef9bd4d6a4e7449444d260140bf84c94a8c3
phun <phun5701@gmail.com>
2025-10-25 20:36:37 -0500

    Stylized text

--- ui/src/app/layouts/auth-layout/auth-layout.ts
+++ ui/src/app/layouts/auth-layout/auth-layout.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgxFlickeringGridComponent } from '@omnedia/ngx-flickering-grid';
+import { ThemeService } from '@core/theme';
+import { NgxBackgroundBeamsComponent } from '@omnedia/ngx-background-beams';
+import { NgxTypewriterComponent } from '@omnedia/ngx-typewriter';
 import { ThemeToggle } from '@shared/components/theme-toggle/theme-toggle';
 import { LayoutModule } from '@shared/components/z-layout/layout.module';
 import { ZardResizableHandleComponent } from '@shared/components/z-resizable/resizable-handle.component';
 import { ZardResizablePanelComponent } from '@shared/components/z-resizable/resizable-panel.component';
 import { ZardResizableComponent } from '@shared/components/z-resizable/resizable.component';
-
 /**
  * Auth layout component.
  */
@@ -21,8 +22,12 @@ import { ZardResizableComponent } from '@shared/components/z-resizable/resizable
     ZardResizableHandleComponent,
     LayoutModule,
     ThemeToggle,
-    NgxFlickeringGridComponent,
+    NgxBackgroundBeamsComponent,
+    NgxTypewriterComponent,
   ],
   templateUrl: './auth-layout.html',
 })
-export class AuthLayout {}
+export class AuthLayout {
+  protected readonly typeWriterWords = ['Secure', 'Peer-to-peer', 'Open', 'GitTor'];
+  protected readonly themeService = inject(ThemeService);
+}
