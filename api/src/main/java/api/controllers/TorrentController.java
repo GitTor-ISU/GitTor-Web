ad5859daffde7508686e90a626cfbbb2ad536133
Tyler <tjgorton23@outlook.com>
2025-11-02 20:21:55 -0600

    Rename RepoController to TorrentController

--- api/src/main/java/api/controllers/TorrentController.java
+++ api/src/main/java/api/controllers/TorrentController.java
@@ -1,6 +1,8 @@
 package api.controllers;
 
+import api.services.S3ObjectService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,28 +10,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * {@link RepoController}.
+ * {@link TorrentController}.
  */
 @RestController
-@RequestMapping("/repos")
+@RequestMapping("/torrents")
 @Tag(
-    name = "Repositories",
-    description = "Repositories contain the project files and the revision history for those files."
+    name = "Torrents",
+    description = "Torrents represent a repository stored in the system."
 )
-public class RepoController {
+public class TorrentController {
+
+    @Autowired
+    private S3ObjectService s3;
 
     @PostMapping("")
-    public void uploadRepo() {
+    public void uploadTorrent() {
         // Implementation goes here
     }
 
     @PutMapping("/{id}")
-    public void updateRepo() {
+    public void updateTorrent() {
         // Implementation goes here
     }
 
     @GetMapping("/{id}")
-    public void getRepo() {
+    public void getTorrent() {
         // Implementation goes here
     }
 }
