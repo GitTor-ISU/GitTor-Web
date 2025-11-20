4a65161853432c949fda8f55edf33f2762953d97
JaysonJAcosta5704 <JaysonJAcosta5704.coding@gmail.com>
2025-11-20 01:13:23 -0500

    Add repository and listing pages, add routes for pages.

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Login } from '@features/auth/login/login';
 import { Register } from '@features/auth/register/register';
 import { Home } from '@features/home/home';
 import { NotFound } from '@features/not-found/not-found';
+import { RepositoryList } from '@features/repository-list/repository-list';
+import { Repository } from '@features/repository/repository';
 import { AuthLayout } from './layouts/auth-layout/auth-layout';
 import { MainLayout } from './layouts/main-layout/main-layout';
 
@@ -14,6 +16,8 @@ export const routes: Routes = [
     children: [
       { path: '', component: Home },
       { path: 'about', component: About },
+      { path: ':owner/repositories', component: RepositoryList },
+      { path: ':owner/:name', component: Repository },
     ],
   },
   {
