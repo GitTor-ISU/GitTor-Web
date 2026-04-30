a5ce05863398c0d9518ffb6e574befa326d88894
phun <125079268+phu-n@users.noreply.github.com>
2026-04-30 13:13:23 -0500

    Added loader back

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import ThemeService from '@core/theme-service';
 import { HeartbeatService } from '@generated/openapi/services/heartbeat';
+import { ZardLoaderComponent } from '@shared/components/z-loader/loader.component';
 import { ZardToastComponent } from '@shared/components/z-toast/toast.component';
 import { toast } from 'ngx-sonner';
 import { firstValueFrom } from 'rxjs';
@@ -11,13 +12,14 @@ import { firstValueFrom } from 'rxjs';
  */
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ZardToastComponent],
+  imports: [RouterOutlet, ZardToastComponent, ZardLoaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   providers: [],
 })
 export class App implements OnInit {
   protected readonly router = inject(Router);
+  protected readonly isLoading = computed(() => !!this.router.currentNavigation());
 
   protected readonly themeService = inject(ThemeService);
   private readonly heartbeatService = inject(HeartbeatService);
