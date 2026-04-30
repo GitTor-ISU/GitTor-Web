09cd88c3ffc41dcd7c4a0d96a3660d69dcc0eca9
phun <125079268+phu-n@users.noreply.github.com>
2026-04-29 21:18:42 -0500

    Stopped getting user every update

--- ui/src/app/shared/user-resolvers.ts
+++ ui/src/app/shared/user-resolvers.ts
@@ -1,11 +1,11 @@
 import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
+import { ResolveFn, Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
 
 import SessionService from '@core/session-service';
 import { TorrentDto } from '@generated/openapi/models/torrent-dto';
-import { UsersService } from '@generated/openapi/services/users';
 import { UserDto } from '@generated/openapi/models/user-dto';
+import { UsersService } from '@generated/openapi/services/users';
 
 export const currentUserResolver: ResolveFn<UserDto | null> = () => {
   const sessionService = inject(SessionService);
@@ -16,9 +16,18 @@ export const currentUserResolver: ResolveFn<UserDto | null> = () => {
 export const profileResolver: ResolveFn<UserDto | null> = async (route) => {
   const usersService = inject(UsersService);
   const sessionService = inject(SessionService);
+  const router = inject(Router);
   const userId = route.paramMap.get('owner');
   if (userId) {
-    const user = await firstValueFrom(usersService.getUser(userId));
+    const user = await firstValueFrom(usersService.getUser(userId))
+      .then((user) => user ?? null)
+      .catch(() => null);
+
+    if (!user) {
+      router.navigate(['404']);
+      return null;
+    }
+
     return {
       ...user,
       isCurrentUser: sessionService.hasToken() && sessionService.user()?.id === user.id,
